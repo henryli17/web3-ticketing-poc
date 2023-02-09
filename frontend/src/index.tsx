2060e6441ced94a3b6a51d1b03e1e897d2691758
Henry Li <henry_li@web.de>
2023-02-09 18:01:34 +0000

    Remove Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement

e18adfa1c531033ca85aa25d8c975b80c8adfcb7
Henry Li <henry_li@web.de>
2023-02-15 00:32:27 +0000

    Formatting

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,12 +3,12 @@ module.exports = {
 		configure: {
 			ignoreWarnings: [
 				function ignoreSourceMapLoaderWarnings(warning) {
-				return (
-					warning.module &&
-					warning.module.resource.includes('node_modules') &&
-					warning.details &&
-					warning.details.includes('source-map-loader')
-				)
+					return (
+						warning.module &&
+						warning.module.resource.includes('node_modules') &&
+						warning.details &&
+						warning.details.includes('source-map-loader')
+					)
 				},
 			],
 		},

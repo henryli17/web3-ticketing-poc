a6e4360633d4755742d960e28936fbd124dd26e8
Henry Li <henry_li@web.de>
2023-02-21 16:31:27 +0000

    Home page

--- frontend/src/views/HomeView.tsx
+++ frontend/src/views/HomeView.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+import routes from "../routes";
+
 const HomeView = () => {
 	return (
-		<div>
-			Home
+		<div className="home-page">
+			<div className="py-32 px-6 flex justify-center text-center">
+				<div>
+					<h1 className="mb-5 text-white text-shadow">
+						Blockchain Based Ticketing
+					</h1>
+					<div className="flex justify-center">
+						<Link to={routes.events()}>
+							<div className="btn text-white">
+								Browse Events
+							</div>
+						</Link>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }

1ed63aa340bff77f9b1f8f97d3117fc2f8d829e1
Henry Li <henry_li@web.de>
2023-02-28 21:07:50 +0000

    Styling

--- frontend/src/components/AlertModal.tsx
+++ frontend/src/components/AlertModal.tsx
@@ -17,12 +17,12 @@ const AlertModal = (props: {
 										<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
 									</svg>
 								</div>
-								<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+								<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
 									<h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">
 										{props.title}
 									</h3>
 									<div className="mt-2">
-										<p className="text-sm text-gray-500">
+										<p className="text-sm text-gray-500 break-words sm:w-[calc(100%-3.5rem)]">
 											{props.message}
 										</p>
 									</div>
--- frontend/src/components/ConfirmationModal.tsx
+++ frontend/src/components/ConfirmationModal.tsx
@@ -29,11 +29,11 @@ const ConfirmationModal = (props: {
 										<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
 									</svg>
 								</div>
-								<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+								<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
 									<h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">
 										{props.title}
 									</h3>
-									<div className="mt-2">
+									<div className="mt-2 break-words sm:w-[calc(100%-3.5rem)]">
 										<p className="text-sm text-gray-500">
 											{props.message}
 										</p>

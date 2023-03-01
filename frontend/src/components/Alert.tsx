77dc2751736f7cbf88b484d54351c4218815b200
Henry Li <henry_li@web.de>
2023-03-01 00:13:48 +0000

    Styling

--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -5,7 +5,7 @@ const Alert = (props: { title?: string, message: string, className?: string }) =
 				<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
 			</svg>
 			<div className="break-words w-[calc(100%-2rem)]">
-				<span className="font-medium">{props.title}</span>
+				<span className="font-medium mr-1">{props.title}</span>
 				{props.message}
 			</div>
 		</div>

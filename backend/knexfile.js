b6584a75864054b959c0200d74583d3ec8641a38
Henry Li <henry_li@web.de>
2023-03-03 18:06:56 +0000

    Deployment bug fixes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,6 +1,6 @@
 module.exports = {
 	client: "sqlite3",
 	connection: {
-		filename: (process.env.NODE_ENV === "production") ? "litefs/db.sqlite3" : "db.sqlite3"
+		filename: (process.env.NODE_ENV === "production") ? "/litefs/db.sqlite3" : "db.sqlite3"
 	}
 };
